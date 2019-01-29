84399e456f84c9f7d204fcbb63ebb97b34e0715b
korneel vandamme <vandamme.korneel@gmail.com>
2019-01-29 12:21:13 +0100

    nummers in array steken + comments

--- web1/javaschript.js
+++ web1/javaschript.js
@@ -5,37 +5,49 @@ document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', checkDoku)
 })
 const checkDoku = function () {
+    let dokuarr = [[], [], [], [], [], [], [], [], []]
     for (let i = 0; i < 9; i++) {
         if (checkelm(i)) {
+            fillarray(dokuarr, i)
         }
     }
 }
-const checknum = function (input,O) {
+
+
+
+const fillarray = function (arr, row) {   // vult de array in met de gegeven gegvens
+    let I = 'I' + row
+    const input = document.getElementById(I).value
+    for (let i = 0; i < 9; i++) {
+        arr[row][i] = input.charAt(i);
+    }
+    console.log(arr)
+}
+const checknum = function (input, O) {    // check of dat er geen charachters in zitten
     for (let i = 0; i < input.length; i++) {
-        if(!checknum1(input,i)){
+        if (!checknum1(input, i)) {
             let rwite = document.getElementById(O)
-        text = rwite.querySelector('p')
-        text.textContent = 'only numbers'
-        text.style.backgroundColor = "red"
-        return false
+            text = rwite.querySelector('p')
+            text.textContent = 'only numbers'
+            text.style.backgroundColor = "red"
+            return false
         }
     }
     return true
-    
 }
-const checknum1 = function(input,j){
-    for (let i = 0; i < 10; i++){
+const checknum1 = function (input, j) {    // check als het gegeven charachter een nummer is
+    for (let i = 0; i < 10; i++) {
         if (input.charAt(j) == i) {
             return true
         }
     }
     return false
 }
-const checkelm = function (i) {
+const checkelm = function (i) { // checkt of het gegeven waarde in een array mag gestoken worden
     let I = 'I' + i
     let O = 'O' + i
     const input = document.getElementById(I).value
-    if (input.length != 9) {
+    if (input.length != 9) {     // checkt of het charachter count 9 is verandert de lay out als nodig is
         let rwite = document.getElementById(O)
         text = rwite.querySelector('p')
         text.textContent = 'needs 9 numbers'
@@ -47,6 +59,7 @@ const checkelm = function (i) {
         rwite = rwite.querySelector('p')
         rwite.textContent = ''
         rwite.style.backgroundColor = "transparent"
-        return checknum(input,O)
+        return checknum(input, O)
     }
 }
+
